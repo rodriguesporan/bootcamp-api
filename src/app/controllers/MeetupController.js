cc5488326233c0d1618611831352745086738ebc
rodriguesporan <rodriguesporan@gmail.com>
2019-07-08 13:30:37 -0300

    Adicionado validações para Meetups

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,8 +1,20 @@
 import { isBefore, parseISO } from 'date-fns';
+import * as Yup from 'yup';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      file_id: Yup.number().required(),
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+    });
+    if (!schema.isValid) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     if (isBefore(parseISO(req.body.date), new Date())) {
       return res
         .status(400)
@@ -15,12 +27,26 @@ class MeetupController {
   }
 
   async update(req, res) {
-    const meetup = await Meetup.findOne({
-      where: { id: req.params.id, user_id: req.userId },
+    const schema = Yup.object().shape({
+      file_id: Yup.number(),
+      title: Yup.string(),
+      description: Yup.string(),
+      location: Yup.string(),
+      date: Yup.date(),
     });
+    if (!schema.isValid) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const meetup = await Meetup.findByPk(req.params.id);
 
     if (!meetup) {
       return res.status(400).json({ error: 'Meetup not found.' });
+    }
+
+    const user_id = req.userId;
+    if (meetup.user_id !== user_id) {
+      return res.status(401).json({ error: 'Not authorized.' });
     }
 
     if (isBefore(parseISO(req.body.date), new Date())) {
