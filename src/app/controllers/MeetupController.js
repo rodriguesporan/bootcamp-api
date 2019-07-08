73beb3662a12282610a098cbbbf63e3b077dcfb0
rodriguesporan <rodriguesporan@gmail.com>
2019-07-08 13:17:25 -0300

    Editando meetups

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -13,6 +13,25 @@ class MeetupController {
     const meetup = await Meetup.create({ ...req.body, user_id });
     return res.json(meetup);
   }
+
+  async update(req, res) {
+    const meetup = await Meetup.findOne({
+      where: { id: req.params.id, user_id: req.userId },
+    });
+
+    if (!meetup) {
+      return res.status(400).json({ error: 'Meetup not found.' });
+    }
+
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res.status(400).json({
+        error: 'You can only update meetups 2 hours until they start.',
+      });
+    }
+
+    await meetup.update(req.body);
+    return res.json(meetup);
+  }
 }
 
 export default new MeetupController();
--- src/routes.js
+++ src/routes.js
@@ -17,5 +17,6 @@ routes.use(authHeader);
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/meetups', MeetupController.store);
+routes.put('/meetups/:id', MeetupController.update);
 
 export default routes;
