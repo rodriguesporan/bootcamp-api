95638c773e00e61283f663137030a7a64d5896b0
rodriguesporan <rodriguesporan@gmail.com>
2019-07-08 16:51:21 -0300

    Adicionando Inscrições

---
+++ src/app/controllers/SubscriptionController.js
@@ -0,0 +1,51 @@
+import Subscription from '../models/Subscription';
+import Meetup from '../models/Meetup';
+
+class SubscriptionController {
+  async store(req, res) {
+    const { meetup_id } = req.body;
+    const meetup = await Meetup.findByPk(meetup_id);
+    if (!meetup) {
+      return res.status(400).json({ error: 'Invalid meetup.' });
+    }
+    const user_id = req.userId;
+    if (user_id === meetup.user_id) {
+      return res
+        .status(400)
+        .json({ error: 'User can not subscribe on your owns meetups.' });
+    }
+    if (meetup.past) {
+      return res.status(400).json({ error: 'Meetup already done.' });
+    }
+    const subscriptionExist = await Subscription.findOne({
+      where: { meetup_id, user_id },
+    });
+    if (subscriptionExist) {
+      return res
+        .status(400)
+        .json({ error: 'You have already been subscribe on this meetup.' });
+    }
+    const checkDate = await Subscription.findOne({
+      where: { user_id },
+      include: [
+        {
+          model: Meetup,
+          required: true,
+          where: {
+            date: meetup.date,
+          },
+        },
+      ],
+    });
+    if (checkDate) {
+      return res.status(400).json({
+        error: `You have alredy subscribed to a meetup at this time ${meetup.date}`,
+      });
+    }
+
+    const subscription = await Subscription.create({ meetup_id, user_id });
+    return res.json(subscription);
+  }
+}
+
+export default new SubscriptionController();
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import authHeader from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import multerConfig from './config/multer';
 import MeetupController from './app/controllers/MeetupController';
+import SubscriptionController from './app/controllers/SubscriptionController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,9 +17,12 @@ routes.post('/sessions', SessionController.store);
 routes.use(authHeader);
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+
 routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:id', MeetupController.update);
 routes.get('/meetups', MeetupController.index);
 routes.delete('/meetups/:id', MeetupController.delete);
+
+routes.post('/subscriptions', SubscriptionController.store);
 
 export default routes;
